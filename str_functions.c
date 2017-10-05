e2a020ea1489ceca911649cf6fb890440b72ec86
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-10-05 17:56:58 -0400

    Add test cases

--- str_functions.c
+++ str_functions.c
@@ -10,33 +10,50 @@ char *stirchr( char *s, char c);
 
 int main() {
 
-    printf("TESTING STIRCPY\n");
+  //printf("TESTING STIRCPY\n");
+
     const char *source = "copyZZZZZ";
     char copyto[] = "aaaaaaaaa";
+    char copytwo[] = "aaaaaaaaa";
+
+    /**
     printf("%s copied to %s ==>", source, copyto);
     printf(" %s\n", stircpy(copyto, source));
 
     printf("\n\n\n\n");
 
     printf("TESTING STIRCMP\n");
+    **/
+
     char str1[] = "abcdefg";
     char str2[] = "abcdefg";
     char str3[] = "baa";
+    char str35[] = "baa";
     char str4[] = "zambonaye";
 
+    /**
     printf("%s compared to %s == %d\n", str1, str2, stircmp(str1, str2));
     printf("%s compared to %s == %d\n", str1, str3, stircmp(str1, str3));
     printf("%s compared to %s == %d\n", str2, str4, stircmp(str2, str4));
     printf("%s compared to %s == %d\n", str4, str3, stircmp(str4, str3));
+    **/
 
-    printf("str1: [%s]\n",  str1);
+    printf("\n\n\nstr1: [%s]\n",  str1);
     printf("str2: [%s]\n",  str2);
     printf("str3: [%s]\n",  str3);
     printf("str4: [%s]\n\n",  str4);
 
+    printf("Testing strcmp(str1, str2):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str1, str2), stircmp(str1, str2));
+
+    printf("Testing strcmp(str1, str3):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str1, str3), stircmp(str1, str3));
+
+    printf("Testing strcmp(str3, str1):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str3, str1), stircmp(str3, str1));
+
+    printf("Testing strcpy('aaaaaaaaa', str1):\n[standard]: %s\n[ours]: %s\n\n", strcpy(copyto, str1), stircpy(copytwo, str1));
+
     printf("Testing strlen(str1):\n[standard]: %d\n[ours]: %d\n\n", strlen(str1), stirlen(str1));
 
-    printf("Testing strcat(str3, str4):\n[standard]: %s\n[ours]: %s\n\n", strcat(str3,str4), stircat(str3, str4));
+    printf("Testing strcat(str3, str4):\n[standard]: %s\n[ours]: %s\n\n", strcat(str3,str4), stircat(str35, str4));
 
     printf("Testing strchr(str1, 'e'):\n[standard]: %p\n[ours]: %p\n\n", strchr(str1, 'e'), stirchr(str1, 'e'));
 
