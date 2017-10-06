21baee2176d5b66bc5e59c8619b4ef992b965408
adrisj7 <adrisj7@gmail.com>
2017-10-05 20:34:39 -0400

    Fix strcpy and organize code. (strcpy also copies over nullpointer)

--- str_functions.c
+++ str_functions.c
@@ -10,55 +10,38 @@ char *stirchr( char *s, char c);
 
 int main() {
 
-  //printf("TESTING STIRCPY\n");
-
-    const char *source = "copyZZZZZ";
-    char copyto[] = "aaaaaaaaa";
-    char copytwo[] = "aaaaaaaaa";
-
-    /**
-    printf("%s copied to %s ==>", source, copyto);
-    printf(" %s\n", stircpy(copyto, source));
-
-    printf("\n\n\n\n");
-
-    printf("TESTING STIRCMP\n");
-    **/
-
     char str1[] = "abcdefg";
     char str2[] = "abcdefg";
-    char str3[] = "baa";
-    char str35[] = "baa";
+    char str3a[] = "baa";
+    char str3b[] = "baa";
     char str4[] = "zambonaye";
 
-    /**
-    printf("%s compared to %s == %d\n", str1, str2, stircmp(str1, str2));
-    printf("%s compared to %s == %d\n", str1, str3, stircmp(str1, str3));
-    printf("%s compared to %s == %d\n", str2, str4, stircmp(str2, str4));
-    printf("%s compared to %s == %d\n", str4, str3, stircmp(str4, str3));
-    **/
+    char copyto1[] = "aaaaaaaaaa";
+    char copyto2[] = "aaaaaaaaaa";
+    char copyto3[] = "aaaaaaaaaa";
 
     printf("\n\n\nstr1: [%s]\n",  str1);
     printf("str2: [%s]\n",  str2);
-    printf("str3: [%s]\n",  str3);
+    printf("str3: [%s]\n",  str3a);
     printf("str4: [%s]\n\n",  str4);
+
 
     printf("Testing strcmp(str1, str2):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str1, str2), stircmp(str1, str2));
 
-    printf("Testing strcmp(str1, str3):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str1, str3), stircmp(str1, str3));
+    printf("Testing strcmp(str1, str3a):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str1, str3a), stircmp(str1, str3a));
 
-    printf("Testing strcmp(str3, str1):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str3, str1), stircmp(str3, str1));
+    printf("Testing strcmp(str3a, str1):\n[standard]: %d\n[ours]: %d\n\n", strcmp(str3a, str1), stircmp(str3a, str1));
 
-    printf("Testing strcpy('aaaaaaaaa', str1):\n[standard]: %s\n[ours]: %s\n\n", strcpy(copyto, str1), stircpy(copytwo, str1));
+    printf("Testing strcpy(%s, str1):\n[standard]: %s\n[ours]: %s\n\n", copyto1, strcpy(copyto2, str1), stircpy(copyto3, str1));
 
-    printf("Testing strlen(str1):\n[standard]: %d\n[ours]: %d\n\n", strlen(str1), stirlen(str1));
+    printf("Testing strlen(str1):\n[standard]: %ld\n[ours]: %d\n\n", strlen(str1), stirlen(str1));
 
-    printf("Testing strcat(str3, str4):\n[standard]: %s\n[ours]: %s\n\n", strcat(str3,str4), stircat(str35, str4));
+    printf("Testing strcat(str3, str4):\n[standard]: %s\n[ours]: %s\n\n", strcat(str3a, str4), stircat(str3b, str4));
 
     printf("Testing strchr(str1, 'e'):\n[standard]: %p\n[ours]: %p\n\n", strchr(str1, 'e'), stirchr(str1, 'e'));
 
     printf("Testing strchr(str1, 'h'):\n[standard]: %p\n[ours]: %p\n\n", strchr(str1, 'h'), stirchr(str1, 'h'));
-    
+
     return 0;
 }
 
@@ -67,6 +50,7 @@ char *stircpy(char *dest, const char *src) {
     for(i = 0; *(src + i); i++) {
         dest[i] = *(src + i);
     }
+    dest[i] = 0;
     return dest;
 }
 
@@ -110,7 +94,7 @@ char * stircat( char * dest, char * src ) {
     int len_src = stirlen(src);
     int len_dest = stirlen(dest);
     int i = 0;
-    for (i; i < len_src; i ++) {
+    for (; i < len_src; i ++) {
       dest[len_dest + i] = src[i];
     }
     dest[len_dest + len_src] = 0;
@@ -121,9 +105,9 @@ char * stircat( char * dest, char * src ) {
 char * stirchr( char * s, char c ) {
     while( * s ) {
         if ( * s == c ) {
-	    return s;
-	}
-	s ++;
+	        return s;
+	    }
+	    s++;
     }
-    //return NULL;
+    return NULL;
 }
